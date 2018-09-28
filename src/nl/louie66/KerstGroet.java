7c9c8f97805ba420635c0dd84a2a8ab876830d68
louie1966 <renedeleeuw66@gmail.com>
2018-09-28 23:51:24 +0200

    Initial commit

---
+++ src/nl/louie66/KerstGroet.java
@@ -0,0 +1,20 @@
+package nl.louie66;
+
+public class KerstGroet {
+    // Dit programma toont een kerstgroet.
+    // @author Rene de Leeuw
+    public static void main(String[] args) {
+        // We tonen tekens die een kerstgroet
+        // laten zien op de console!
+
+        System.out.println("Merry Christmas!");
+        System.out.println();
+        System.out.println("     *     ");
+        System.out.println("   * * *   ");
+        System.out.println("    * *    ");
+        System.out.println("  * * * *  ");
+        System.out.println("   * * *   ");
+        System.out.println(" * * * * * ");
+        System.out.println("     *     ");
+    }
+}
